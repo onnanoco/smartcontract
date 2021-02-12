36ceac4a0441de295a36ed3367b3175303e31548
onnanoco <moe@onnano.co>
2021-02-13 05:46:34 +0900

    Create MoeToken.sol

---
+++ contracts/MoeToken.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.0 < 0.8.0;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+
+contract MoeToken is Initializable, ERC20Upgradeable {
+
+    
+
+}
