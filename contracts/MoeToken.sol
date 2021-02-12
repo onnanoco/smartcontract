19460d8c6aba9be20d492fce6cdb13acae31e934
onnanoco <moe@onnano.co>
2021-02-13 05:55:22 +0900

    Update MoeToken.sol

--- contracts/MoeToken.sol
+++ contracts/MoeToken.sol
@@ -2,9 +2,14 @@
 pragma solidity >=0.6.0 < 0.8.0;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/Initializable.sol";
 
 contract MoeToken is Initializable, ERC20Upgradeable {
 
+    function initialize(string memory name, string memory symbol, uint256 initialSupply) public virtual initializer {
+        __ERC20_init(name, symbol);
+        _mint(_msgSender(), initialSupply);
     
+    }
 
 }
